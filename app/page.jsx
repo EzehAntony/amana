a9d3d721b20a5fcc150f572e5ea8a30437ef317a
Anthony <crayonno.o@gmail.com>
2023-01-24 02:16:54 +0100

    m

--- app/page.jsx
+++ app/page.jsx
@@ -1,91 +1,91 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.jsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.hero}>
+        <header>
+          <img src="/logo.png" alt="" />
+          <nav>
+            <li className={inter.className}>home</li>
+            <li className={inter.className}>pricing</li>
+            <li className={inter.className}>portfolio</li>
+            <li className={inter.className}>contact us</li>
+          </nav>
+
+          <button className={inter.className}>get started</button>
+        </header>
+
+        <h1 className={inter.className}>
+          Grow your business with digital marketing
+        </h1>
+
+        <img src="/vector.png" alt="" />
+
+        <div className={styles.buttonContainer}>
+          <button>get started</button>
+          <img src="/play.png" alt="" />
+        </div>
+      </div>
+      <div className={styles.people}>
+        <img src="/people.png" alt="" />
+      </div>
+
+      <div className={styles.services}>
+        <span className={inter.className}>Our services</span>
+        <p className={inter.className}>what we do </p>
+
+        <div className={styles.servicesContainer}>
+          <div>
+            <h2 className={inter.className}>grow your business</h2>
+            <p className={inter.className}>
+              we have a team of professionals who will help you to grow your
+              business and also collaborate with you.
+            </p>
+          </div>
+          <div>
+            <h2 className={inter.className}>best tools</h2>
+            <p className={inter.className}>
+              we use the best tools to help you drive more sales for your
+              business
+            </p>
+          </div>
+          <div>
+            <h2 className={inter.className}>drive more sales</h2>
+            <p className={inter.className}>
+              we use the best tools to help you drive more sales for your
+              business
+            </p>
+          </div>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+      <div className={styles.offer}>
+        <img src="/woman.png" alt="" />
+        <div className={styles.right}>
+          <h2 className={inter.className}>
+            We offer the best services to grow your business
+          </h2>
+          <p className={inter.className}>
+            We have a team of professionals who will help you to grow your
+            business and also collaborate with you.
+          </p>
+          <button>learn more</button>
         </div>
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className={styles.testimonials}>
+        <span className={styles.className}>testimonials</span>
+        <h2 className={styles.className}>Our client world</h2>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className={styles.cardWrapper}>
+          <div className={styles.card}></div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
