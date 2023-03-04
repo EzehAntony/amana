4167e76c4fbac205bfee8d379e1c4ef124206a12
Anthony <crayonno.o@gmail.com>
2023-03-04 07:20:21 +0100

    gsap

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Image from "next/image";
 import { Inter, Nosifer } from "@next/font/google";
 import styles from "./page.module.css";
+import gsap from "gsap";
+import { Power4, Power3, Power0, Power1, Power2 } from "gsap";
+import { useLayoutEffect, useRef } from "react";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 const inter = Inter({ subsets: ["latin"] });
 const nosifer = Nosifer({
@@ -9,11 +15,142 @@ const nosifer = Nosifer({
 });
 
 export default function Home() {
+  gsap.registerPlugin(ScrollTrigger);
+  const ref = useRef();
+  const g = gsap.utils.selector(ref);
+  const t1 = gsap.timeline({
+    defaults: {
+      opacity: 0,
+    },
+  });
+
+  const t2 = gsap.timeline({ defaults: { opacity: 0 } });
+  const t3 = gsap.timeline({ defaults: { opacity: 0 } });
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.to(g("#main *"), { visibility: "visible" });
+      const hero = (() => {
+        t1.from(g("#logo"), { x: 40 })
+          .from(
+            g("#hero h1"),
+            {
+              y: 50,
+            },
+            "<+=.2"
+          )
+          .from(g("#hero #vector"), {}, "<+=.5")
+          .from(g("#hero button"), { y: -20, ease: "bounce" }, "<+=.2")
+          .from(g("#hero #play"), { x: -20, ease: "bounce" }, "<+=.3")
+          .from(
+            g("#people"),
+            {
+              scale: 1.5,
+              y: -20,
+              ease: Power1.easeIn,
+            },
+            "<+=.3"
+          );
+      })();
+
+      const services = (() => {
+        t2.from(g("#services > span"), {
+          y: 50,
+          scrollTrigger: {
+            trigger: "#services > span",
+            start: "top 80%",
+            end: "top 70%",
+            scrub: 1,
+          },
+        }).from(g("#services > p"), {
+          y: 50,
+          scrollTrigger: {
+            trigger: "#services >p",
+            start: "top 80%",
+            end: "top 70%",
+            scrub: 1,
+          },
+        });
+        for (var i = 0; i < 5; i++) {
+          t2.from(g(`#services div div:nth-of-type(${i})`), {
+            x: 50,
+            scrollTrigger: {
+              trigger: `#services div div:nth-of-type(${i})`,
+              start: "top 80%",
+              end: "top 70%",
+              scrub: 1,
+            },
+          });
+          t2.from(g(`#services div div:nth-of-type(${i}) h2`), {
+            y: 50,
+            scrollTrigger: {
+              trigger: `#services div div:nth-of-type(${i}) h2`,
+              start: "top 80%",
+              end: "top 70%",
+              scrub: 1,
+            },
+          });
+          t2.from(g(`#services div div:nth-of-type(${i}) p`), {
+            y: 50,
+            scrollTrigger: {
+              trigger: `#services div div:nth-of-type(${i}) p`,
+              start: "top 80%",
+              end: "top 70%",
+              scrub: 1,
+            },
+          });
+        }
+      })();
+
+      const offer = (() => {
+        t3.from(g("#offer img"), {
+          scale: 1.5,
+          scrollTrigger: {
+            trigger: "#offer img",
+            start: "top 80%",
+            end: "top 60%",
+            scrub: 1,
+          },
+        })
+          .from(g("#offer h2"), {
+            x: -50,
+            scrollTrigger: {
+              trigger: "#offer h2",
+              start: "top 80%",
+              end: "top 70%",
+              scrub: 1,
+            },
+          })
+          .from(g("#offer p"), {
+            y: 50,
+            scrollTrigger: {
+              trigger: "#offer p",
+              start: "top 80%",
+              end: "top 70%",
+              scrub: 1,
+            },
+          })
+          .from(g("#offer button"), {
+            y: 50,
+            scrollTrigger: {
+              trigger: "#offer button",
+              start: "top 80%",
+              end: "top 70%",
+              scrub: 1,
+            },
+          });
+      })();
+    });
+    return () => {
+      ctx.revert();
+    };
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.hero}>
-        <header>
-          <img src="/logo.png" alt="" />
+    <main className={styles.main} id="main" ref={ref}>
+      <div id="hero" className={styles.hero}>
+        <header id="header">
+          <img id="logo" src="/logo.png" alt="" />
           <nav>
             <li className={inter.className}>home</li>
             <li className={inter.className}>pricing</li>
@@ -28,18 +165,18 @@ export default function Home() {
           Grow your business with digital marketing
         </h1>
 
-        <img src="/vector.png" alt="" />
+        <img id="vector" src="/vector.png" alt="" />
 
         <div className={styles.buttonContainer}>
           <button>get started</button>
-          <img src="/play.png" alt="" />
+          <img id="play" src="/play.png" alt="" />
         </div>
       </div>
       <div className={styles.people}>
-        <img src="/people.png" alt="" />
+        <img id="people" src="/people.png" alt="" />
       </div>
 
-      <div className={styles.services}>
+      <div className={styles.services} id={"services"}>
         <span className={inter.className}>Our services</span>
         <p className={inter.className}>what we do</p>
 
@@ -68,7 +205,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className={styles.offer}>
+      <div id="offer" className={styles.offer}>
         <img src="/woman.png" alt="" />
         <div className={styles.right}>
           <h2 className={inter.className}>
